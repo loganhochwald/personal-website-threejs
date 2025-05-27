7905cb9cd6af62d8b428d2b9127c419889f8d939
Logan Hochwald <loganhochwald@gmail.com>
2025-05-27 15:44:48 -0500

    feat: init project with tailwind and eslint prettier

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen bg-black flex items-center justify-center">
+      <h1 className="text-white text-6xl font-bold">Logan Hochwald</h1>
+    </div>
+  );
+}
+
+export default App;
